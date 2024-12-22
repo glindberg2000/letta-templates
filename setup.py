208c3e93ad5407c9011e9041ed6ff77f0e2c4649
Greg Lindberg <greglindberg@gmail.com>
2024-12-22 08:21:50 +0000

    merge: add pydantic and python-dotenv dependencies

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
         "letta>=0.6.6",
         "grpcio-tools>=1.68.1",
         "protobuf>=5.26.1,<6.0dev",
+        "pydantic",
+        "python-dotenv"
     ],
     author="LettaDev",
     description="Templates and tools for Letta AI server",
