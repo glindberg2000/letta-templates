cfcc5f3916f3a2bcf24ffbd481cdf3790228d891
Greg Lindberg <greglindberg@gmail.com>
2024-12-24 20:01:06 +0000

    Update all version numbers to 0.2.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="letta_templates",
-    version="0.2.6",
+    version="0.2.6",  # Navigation system update
     description="Templates and tools for Letta AI server",
     author="LettaDev",
     packages=find_packages(),
