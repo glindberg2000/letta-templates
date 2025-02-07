053085bca1f4e61ffe522ef117ff097eb0eb3215
G. Lindberg <greglindberg@gmail.com>
2025-02-07 14:59:24 -0800

    Moved letta_templates directory to root level

--- letta-v2/README.md
+++
@@ -1,5 +0,0 @@
-# Letta Templates
-
-Letta NPC Templates and Utilities for Roblox Game Development.
-
-Version: 2.1.0 
--- letta-v2/test_imports.py
+++
@@ -1,8 +0,0 @@
-from letta_templates.npc_utils_v2 import (
-    update_location_status,
-    update_group_members_v2,
-    get_location_history,
-    get_group_history
-)
-
-print("Successfully imported all functions!")
